85d4d98805291acbbb79d7682fbc7de3ca091ff8
Lorenzo Del Re <136103234+LoreWebDev24@users.noreply.github.com>
2023-12-11 17:35:22 +0100

    dynamic product (comics) displayed in the page

--- resources/views/partials/main.blade.php
+++ resources/views/partials/main.blade.php
@@ -1,5 +1,14 @@
 <section class="app-main">
     <div class="container">
-        <p>Main content</p>
-    </div>
+        <div class="row">
+            @foreach ($comics as $comic)
+                <div class="col-2">
+                    <img src="{{ $comic['thumb'] }}" alt="cover-fumetto">
+                    <span><strong>{{ $comic['title'] }}</strong></span>
+                </div>
+            @endforeach
+            <div class="pseudo-button">
+                <span><strong><a href="#">LOAD MORE</a></strong></span>
+            </div>
+        </div>
 </section>
